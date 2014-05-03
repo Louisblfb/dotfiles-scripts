e0a6c3361d407f9c7f72b55e1f524a98ef032234
Louis Belford <louisblfd@gmail.com>
2014-05-03 19:20:21 +1000

    modified:   current-mpd.py

--- current-mpd.py
+++ current-mpd.py
@@ -7,27 +7,17 @@ except ImportError:
 import socket
 import sys
 
-
 HOST = 'localhost'
 PORT = 6600
 RECV_SIZE = 2**20 # One megabyte
 try:
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  print 'Connecting to %s:%d...' % (HOST, PORT)
   s.connect((HOST, PORT))
 except socket.error:
-  print 'Unable to connect.'
   sys.exit(1)
 
 print s.recv(RECV_SIZE)
-print '*** MPD-Console ***\n'
-
-
-
-cmd = 'currentsong'
-s.send(cmd + '\n')
-print s.recv(RECV_SIZE)
-  
-print 
+s.send('currentsong' + '\n')
+print s.recv(RECV_SIZE)  
 s.close()
 
